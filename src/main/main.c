f458e52ff802842fae16de4ad18c5457be467c77
Justin Handville <nanolith@gmail.com>
2022-05-07 10:15:09 -0400

    Stubbed out main and cmake.

---
+++ src/main/main.c
@@ -0,0 +1,21 @@
+/**
+ * \file main/main.c
+ *
+ * \brief Main entry point for weightgraph.
+ *
+ * \copyright 2022 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <stdio.h>
+#include <weightgraph/config.h>
+
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    printf("Feature not yet implemented.\n");
+
+    return 1;
+}
